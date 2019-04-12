0fdf5a25221699b40ac0c1a7f324911235d0e70d
zhaoliang <>
2019-04-12 11:13:55 +0800

    示例场景代码优化

--- Assets/ZL_SimpleMsg/Example/Sender.cs
+++ Assets/ZL_SimpleMsg/Example/Sender.cs
@@ -5,12 +5,13 @@ public class Sender : MonoBehaviour, IMsgSender
 {
     void Update()
     {
-        //MsgParamObject msgParamObject = new MsgParamObject();
-        MsgParam<string> msgParam = new MsgParam<string>();
-        msgParam.SetParam("Hello World");
-
         if (Input.GetMouseButtonDown(0))
+        {
+            //MsgParamObject msgParamObject = new MsgParamObject();
+            MsgParam<string> msgParam = new MsgParam<string>();
+            msgParam.SetParam("Hello World");
             this.SendLogicMsg(MsgName.MSG_TESTMSGNAME, msgParam);
+        }
     }
 
     private void TestAction()
